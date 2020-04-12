632daf4a078529810be1cadc672f12882507ad46
Cristian Golea <c.golea@evision-software.com>
2020-04-12 13:49:00 +0200

    Add playground for database project(what to do with it?)

---
+++ Database1/Employees.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FistName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [DateOfBirth] DATE NULL, 
+    [Height] INT NULL
+)

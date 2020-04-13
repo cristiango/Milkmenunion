33f8c74c3bcc4eb4aa64f62fb97fb5c1b7f66e83
Cristian Golea <c.golea@evision-software.com>
2020-04-13 13:23:15 +0200

    Remove leftover test

--- Database1/Employees.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Employees]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [FistName] NVARCHAR(50) NULL, 
-    [LastName] NVARCHAR(50) NULL, 
-    [DateOfBirth] DATE NULL, 
-    [Height] INT NULL
-)
